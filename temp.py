68ebb0ccd1bab89ab913d53bd39445e4dfe5d18b
ckh22 <45274762+ckh22@users.noreply.github.com>
2020-12-15 17:09:19 -0800

    changes

--- temp.py
+++ temp.py
@@ -1,26 +1,25 @@
 import json
 from flask import Flask, request, redirect, g, render_template
 import requests
-from urllib.parse import quote
+from urllib.parse import quote, urlencode
 
-# Authentication Steps, paramaters, and responses are defined at https://developer.spotify.com/web-api/authorization-guide/
-# Visit this url to see all the steps, parameters, and expected response.
+from werkzeug.datastructures import Headers
 
+def pretty(obj):
+    return json.dumps(obj, sort_keys=True, indent=2)
 
 app = Flask(__name__)
 
-#  Client Keys
 CLIENT_ID = "c3489fd7fba04530a443381d740d1dc2"
 CLIENT_SECRET = "2a8ad0b1aa4c4a9ba40c673397e9b7d5"
 
-# Spotify URLS
 SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
 SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
 SPOTIFY_API_BASE_URL = "https://api.spotify.com"
 API_VERSION = "v1"
 SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
 
-# Server-side Parameters
+
 CLIENT_SIDE_URL = "http://127.0.0.1"
 PORT = 8080
 REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
@@ -33,15 +32,24 @@ auth_query_parameters = {
     "response_type": "code",
     "redirect_uri": REDIRECT_URI,
     "scope": SCOPE,
-    # "state": STATE,
-    # "show_dialog": SHOW_DIALOG_str,
     "client_id": CLIENT_ID
 }
+
+class Playlist():
+    def __init__(self, playlist, authorization):
+        self.playlist = playlist
+        self.name = playlist['name']
+        self.id = playlist['id']
+        self.image = playlist['images'][0]['url']
+        self.owner = playlist['owner']['display_name']
+        self.tracks = json.loads(requests.get(playlist['tracks']['href'], headers=authorization).text)
+
+    def __str__(self):
+        return json.dumps(self.tracks, indent=4)
 
 
 @app.route("/")
 def index():
-    # Auth Step 1: Authorization
     url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
     auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
     return redirect(auth_url)
@@ -49,7 +57,6 @@ def index():
 
 @app.route("/callback/q")
 def callback():
-    # Auth Step 4: Requests refresh and access tokens
     auth_token = request.args['code']
     code_payload = {
         "grant_type": "authorization_code",
@@ -59,31 +66,26 @@ def callback():
         'client_secret': CLIENT_SECRET,
     }
     post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
-
-    # Auth Step 5: Tokens are Returned to Application
     response_data = json.loads(post_request.text)
     access_token = response_data["access_token"]
     refresh_token = response_data["refresh_token"]
     token_type = response_data["token_type"]
     expires_in = response_data["expires_in"]
-    print(access_token)
-
-    # Auth Step 6: Use the access token to access Spotify API
     authorization_header = {"Authorization": "Bearer {}".format(access_token)}
-
-    # Get profile data
     user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
     profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
     profile_data = json.loads(profile_response.text)
-
-    # Get user playlist data
     playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
     playlists_response = requests.get(playlist_api_endpoint, headers=authorization_header)
     playlist_data = json.loads(playlists_response.text)
-
-    # Combine profile and playlist data to display
-    display_arr = [profile_data] + playlist_data["items"]
-    return render_template("landing.html", sorted_array=display_arr)
+    playlists = []
+    for playlist in playlist_data['items']:
+        playlist_id = playlist['id']
+        pl_URL = 'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'.format(playlist_id=playlist_id)
+        tracks_response =requests.get(pl_URL, headers=authorization_header)
+        tracks_data = json.loads(tracks_response.text)
+        playlists.append(Playlist(playlist=playlist, authorization=authorization_header))
+    return render_template("landing.html", playlists=playlists)
 
 
 if __name__ == "__main__":
