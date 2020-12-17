813c645b8422be1f47654d75dea1f94b00255621
ckh22 <45274762+ckh22@users.noreply.github.com>
2020-12-16 20:48:27 -0800

    working

--- temp.py
+++ temp.py
@@ -1,5 +1,7 @@
+from requests.sessions import session
+from finalproject import Song
 import json
-from flask import Flask, request, redirect, g, render_template
+from flask import Flask, request, redirect, g, render_template, session
 import requests
 from urllib.parse import quote, urlencode
 
@@ -9,6 +11,7 @@ def pretty(obj):
     return json.dumps(obj, sort_keys=True, indent=2)
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 CLIENT_ID = "c3489fd7fba04530a443381d740d1dc2"
 CLIENT_SECRET = "2a8ad0b1aa4c4a9ba40c673397e9b7d5"
@@ -28,6 +31,9 @@ STATE = ""
 SHOW_DIALOG_bool = True
 SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
 
+authorization_header = None
+playlists = {}
+
 auth_query_parameters = {
     "response_type": "code",
     "redirect_uri": REDIRECT_URI,
@@ -35,13 +41,18 @@ auth_query_parameters = {
     "client_id": CLIENT_ID
 }
 
+
 class Track():
     def __init__(self, track):
         self.name = track['track']['name']
         self.track = track
+        artistList = []
+        for artist in track['track']['album']['artists']:
+            artistList.append(artist['name'])
+        self.artists = artistList
     
     def __str__(self):
-        return self.name
+        return self.track
 
 
 class Playlist():
@@ -51,9 +62,10 @@ class Playlist():
         self.id = playlist['id']
         self.image = playlist['images'][0]['url']
         self.owner = playlist['owner']['display_name']
-        tracks = json.loads(requests.get(playlist['tracks']['href'], headers=authorization).text)
+        tracks = requests.get(playlist['tracks']['href'], headers=authorization)
+        track_data = json.loads(tracks.text)
         trackList = []
-        for track in tracks['items']:
+        for track in track_data['items']:
             trackList.append(Track(track))
         self.tracks = trackList
 
@@ -91,15 +103,16 @@ def callback():
     playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
     playlists_response = requests.get(playlist_api_endpoint, headers=authorization_header)
     playlist_data = json.loads(playlists_response.text)
-    playlists = []
     for playlist in playlist_data['items']:
         playlist_id = playlist['id']
-        pl_URL = 'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'.format(playlist_id=playlist_id)
-        tracks_response =requests.get(pl_URL, headers=authorization_header)
-        tracks_data = json.loads(tracks_response.text)
-        playlists.append(Playlist(playlist=playlist, authorization=authorization_header))
-    return render_template("landing.html", playlists=playlists)
+        playlists[playlist_id] = Playlist(playlist=playlist, authorization=authorization_header)
+    return render_template("landing.html", playlists=playlists.values())
 
+@app.route('/playlist/<playlist_id>')
+def playlist(playlist_id):
+    if playlist_id in playlists:
+        print(pretty(playlists[playlist_id].tracks[0].track))
+        return render_template("playlist.html", playlist=playlists[playlist_id])
 
 if __name__ == "__main__":
     app.run(debug=True, port=PORT)
